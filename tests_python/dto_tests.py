32f87416131d2ce14e87dca492e40cf14b91125b
Kelp <kelp@phate.org>
2014-02-21 18:15:05 +0800

    added tests for dto.user_model

--- tests_python/dto_tests.py
+++ tests_python/dto_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from mock import MagicMock
 from application.models.dto.user_model import *
 
 
@@ -7,3 +8,19 @@ class UserPermissionTest(unittest.TestCase):
         self.assertEqual(UserPermission.anonymous, 0)
         self.assertEqual(UserPermission.root, 1)
         self.assertEqual(UserPermission.normal, 2)
+
+class UserModelTest(unittest.TestCase):
+    def setUp(self):
+        # mock google.appengine.api.users
+        from google.appengine.api import users
+        users.create_login_url = MagicMock(return_value='login_url')
+
+    def test_user_model_default(self):
+        model = UserModel()
+        self.assertFalse(model.is_login)
+        self.assertEqual(model.id, 0)
+        self.assertEqual(model.permission, UserPermission.anonymous)
+        self.assertEqual(model.name, 'Guest')
+        self.assertEqual(model.email, '')
+        self.assertEqual(model.login_url, 'login_url')
+
