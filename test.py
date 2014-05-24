183e8c23f39692d8e1272a6d41a5d1d075ee6e28
Kelp <kelp@phate.org>
2014-05-24 10:44:51 +0800

    updated test.

--- test.py
+++ test.py
@@ -1,26 +1,17 @@
 import os, sys, unittest
 
 
-if os.path.isdir('./google_appengine'):
-    gae_path = './google_appengine'
-else:
-    gae_path = '/usr/local/google_appengine'
-
-sys.path.append(gae_path)
-libs = [
-    os.path.join(gae_path, 'google'),
-    os.path.join(gae_path, 'lib', 'yaml', 'lib'),
-]
-sys.path.extend(libs)
-
-
 if __name__ == '__main__':
-    tests_dir = 'tests_python'
-    test_modules = ['%s.' % tests_dir + filename.replace('.py', '') for filename in os.listdir('./%s' % tests_dir)
-                  if filename.endswith('.py') and not filename.startswith('__')]
-    map(__import__, test_modules)
+    # gae
+    if os.path.isdir('./google_appengine'):
+        gae_path = './google_appengine'
+    else:
+        gae_path = '/usr/local/google_appengine'
+    sys.path.append(gae_path)
+    sys.path.extend([
+        os.path.join(gae_path, 'google'),
+        os.path.join(gae_path, 'lib', 'yaml', 'lib'),
+    ])
 
-    suite = unittest.TestSuite()
-    for mod in [sys.modules[modname] for modname in test_modules]:
-        suite.addTest(unittest.TestLoader().loadTestsFromModule(mod))
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    tests = unittest.TestLoader().discover('tests_python')
+    unittest.TextTestRunner(verbosity=2).run(tests)
